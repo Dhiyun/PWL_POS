020de173a4b7c7168472f869045c7c77c09aeb49
Dhiyun <dhkcraft147@gmail.com>
2024-03-06 10:00:52 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,18 +10,9 @@ class UserController extends Controller
 {
     public function index()
     {
-        // Tambah data user dengan Eloquent Model
-        $data = 
-        [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::CREATE($data); // Tambahkan data ke tabel m_user
-
-        // Coba akses model UserModel
-        $user = UserModel::all(); // Ambil semua data dari tabel m_user;
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
