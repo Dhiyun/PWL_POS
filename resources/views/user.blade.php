e8a1b1458cf94d455dc3cf700ae7e7cb37566667
Dhiyun <dhkcraft147@gmail.com>
2024-03-06 10:15:41 +0700

    Retreiving Aggregrates

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,16 +10,18 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
+            {{-- <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <th>ID Level Pengguna</th> --}}
+            <th>Jumlah Pengguna</th>
         </tr>
         <tr>
-            <th>{{ $data->user_id }}</th>
+            {{-- <th>{{ $data->user_id }}</th>
             <th>{{ $data->username }}</th>
             <th>{{ $data->nama }}</th>
-            <th>{{ $data->level_id }}</th>
+            <th>{{ $data->level_id }}</th> --}}
+            <th>{{ $data }}</th>
         </tr>
     </table>
 </body>
