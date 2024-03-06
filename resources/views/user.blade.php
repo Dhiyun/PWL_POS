2f989525be2f40661a58fe6c5dfb92f00cd06793
Dhiyun <dhkcraft147@gmail.com>
2024-03-06 10:37:11 +0700

    Retreiving or Creating Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,18 +10,16 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            {{-- <th>ID</th>
+            <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th> --}}
-            <th>Jumlah Pengguna</th>
+            <th>ID Level Pengguna</th>
         </tr>
         <tr>
-            {{-- <th>{{ $data->user_id }}</th>
+            <th>{{ $data->user_id }}</th>
             <th>{{ $data->username }}</th>
             <th>{{ $data->nama }}</th>
-            <th>{{ $data->level_id }}</th> --}}
-            <th>{{ $data }}</th>
+            <th>{{ $data->level_id }}</th>
         </tr>
     </table>
 </body>
