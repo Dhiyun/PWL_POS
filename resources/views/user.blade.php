a873ac0a50917283dc37b441cb768472c4d8660b
Dhiyun <dhkcraft147@gmail.com>
2024-03-08 00:25:15 +0700

    Create, Read, Update, Delete (CRUD)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -8,19 +8,27 @@
 </head>
 <body>
     <h1>Data User</h1>
+    <h3><a href="/user/tambah"> + Tambah Data</a></h3>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Nama</th>
-            <th>ID Level Pengguna</th>
+            <td>ID</td>
+            <td>Username</td>
+            <td>Nama</td>
+            <td>ID Level Pengguna</td>
+            <td>Aksi</td>
         </tr>
+        @foreach ($data as $d)
         <tr>
-            <th>{{ $data->user_id }}</th>
-            <th>{{ $data->username }}</th>
-            <th>{{ $data->nama }}</th>
-            <th>{{ $data->level_id }}</th>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+            <td>
+                <a href="/user/ubah/{{ $d->user_id }}">Ubah</a>
+                <a href="/user/hapus/{{ $d->user_id }}">hapus</a>
+            </td>
         </tr>
+        @endforeach
     </table>
 </body>
 </html>
